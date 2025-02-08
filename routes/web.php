a607aeb1fd753872550cd608fbd63963b3a60d52
jithinlal <jithtitan007@gmail.com>
2025-02-08 18:28:27 +0530

    feat: laravel pint setup

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::get('/', function () {
 	]);
 });
 
-
 Route::get('/dashboard', function () {
 	return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
