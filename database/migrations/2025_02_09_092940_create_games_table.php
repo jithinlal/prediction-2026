9bcfd6024a59769f88725c10b6ef376a3c0597b3
jithinlal <jithtitan007@gmail.com>
2025-02-09 15:17:36 +0530

    feat: initial db setup and relationships

---
+++ database/migrations/2025_02_09_092940_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('games', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('home_team_id')->constrained('teams')->cascadeOnDelete();
+			$table->foreignId('away_team_id')->constrained('teams')->cascadeOnDelete();
+			$table->foreignId('group_id')->nullable()->constrained('groups')->cascadeOnDelete();
+			$table->dateTime('date');
+			$table->string('stadium');
+			$table->integer('home_goals')->default(0);
+			$table->integer('away_goals')->default(0);
+			$table->integer('home_penalty_goals')->default(0);
+			$table->integer('away_penalty_goals')->default(0);
+			$table->boolean('is_knockout')->default(false);
+			$table->boolean('is_winner_bonus')->default(false);
+			$table->boolean('is_score_bonus')->default(false);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('games');
+	}
+};
